b1b8fe0ec700065aeaf46dc06952c9bbb8062dad
Andrzej <andrzejjac@gmail.com>
2016-03-19 04:15:44 +0100

    added lessons

--- server/classlist_server/backend/models.py
+++ server/classlist_server/backend/models.py
@@ -11,10 +11,44 @@ class Student(models.Model):
         return len(Lesson.objects.all())
 
     @property
+    def get_absent_lessons(self):
+        toReturn = ""
+        for lesson in Lesson.objects.exclude(students__id=self.id):
+            toReturn += str(lesson)  + "<br>"
+        if toReturn == "":
+            return "This student was present at every lesson"
+        return toReturn
+
+    @property
     def get_present_lessons_num(self):
         return len(Lesson.objects.filter(students__id=self.id))
+
+    @property
+    def get_present_lessons(self):
+        toReturn = ""
+        for lesson in Lesson.objects.filter(students__id=self.id):
+            toReturn += str(lesson) + "<br>"
+        if toReturn == "":
+            return "This student was absent at every lesson"
+        return toReturn
 
 class Lesson(models.Model):
     name = models.CharField(max_length=200)
     img = models.ImageField(upload_to="files/")
     students = models.ManyToManyField(Student)
+    dateTime = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_present_students_num(self):
+        return len(self.students.all())
+
+    @property
+    def get_absent_students_num(self):
+        return len(Student.objects.all()) - len(self.students.all())
+
+    @property
+    def my_str(self):
+        return str(self)
+
+    def __str__(self):
+        return "<a href='#'>" + str(self.name) + '</a>'
--- server/classlist_server/frontend/views.py
+++ server/classlist_server/frontend/views.py
@@ -1,7 +1,7 @@
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
-from backend.models import Student
+from backend.models import Student, Lesson
 
 def index(request):
     return HttpResponse(render(request, 'index.html'))
@@ -13,7 +13,10 @@ def students(request):
     return HttpResponse(render(request, 'students.html', context))
 
 def lessons(request):
-    return HttpResponse(render(request, 'lessons.html'))
+    context = {}
+    context["lessons"] = Lesson.objects.all()
+
+    return HttpResponse(render(request, 'lessons.html', context))
 
 def how_it_works(request):
     return HttpResponse(render(request, 'how_it_works.html'))
