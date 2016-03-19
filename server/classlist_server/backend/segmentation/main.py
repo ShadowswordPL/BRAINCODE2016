3dcd229fed7e892b02f8756ad01afaa42a6cc419
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2016-03-19 03:41:34 +0100

    Naprawiona segmentacja.

--- server/classlist_server/backend/segmentation/main.py
+++ server/classlist_server/backend/segmentation/main.py
@@ -3,43 +3,22 @@ import sys
 
 from PIL import Image
 
+from segment import detect_faces
+
 def main():
     # Get user supplied values
     imagePath = sys.argv[1]
-    cascPath = sys.argv[2]
 
-    faces, height, width = detect_faces(imagePath, "haarcascade_frontalface_default.xml", 1.02)
+    faces, _, _ = detect_faces(imagePath, "haarcascade_frontalface_default.xml", 1.02)
     print "Found {0} faces!".format(len(faces))
 
     # Draw a rectangle around the faces
-    i = 0
-    change = 20
-
     image = cv2.imread(imagePath)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     cv2.imshow("Faces found", image)
-    for (x, y, w, h) in faces:
-        i += 1
-        im = Image.open(imagePath)
-        crop_tuple = (max(x - change, 0), max(y - change, 0), min(x + w + change, width), min(y + h + change, height))
-        im = im.crop(crop_tuple)
-        faceImagePath = 'photos/%d.png' % i
-        im.save(faceImagePath)
-
-        #image2 = cv2.imread(faceImagePath)
-        #faces2, height2, width2 = detect_faces(faceImagePath, "haarcascade_frontalface_default.xml", 1.01)
-
-        faceImagePath2 = 'photos_results/%d.png' % i
-
-        #if len(faces2) > 0:
-        im.save(faceImagePath2)
-
-        #for (x, y, w, h) in faces2:
-        #    cv2.rectangle(image2, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        #cv2.imshow("Faces found", image2)
-        #cv2.waitKey(0)
-
-    cv2.imshow("Faces found", image)
     cv2.waitKey(0)
+
+if __name__ == "__main__":
+    main()
--- server/classlist_server/backend/segmentation/segment.py
+++ server/classlist_server/backend/segmentation/segment.py
@@ -1,8 +1,14 @@
 import cv2
+import os
+import time
+from PIL import Image
 
 def detect_faces(imagePath, cascPath='haarcascade_frontalface_default.xml', scaleFactor=1.02):
     # Create the haar cascade
-    faceCascade = cv2.CascadeClassifier(cascPath)
+    faceCascade = cv2.CascadeClassifier('backend/segmentation/' + cascPath)
+    print os.getcwd()
+    print '/home/tommalla/Programowanie/Python/BrainCode/BRAINCODE2016/server/classlist_server/backend/segmentation/' + cascPath
+    print faceCascade.empty()
 
     # Read the image
     image = cv2.imread(imagePath)
@@ -17,4 +23,17 @@ def detect_faces(imagePath, cascPath='haarcascade_frontalface_default.xml', scal
     )
 
     print "Found {0} faces".format(len(faces))
+
+    i = 0
+    change = 20
+
+    for (x, y, w, h) in faces:
+        i += 1
+        im = Image.open(imagePath)
+        crop_tuple = (max(x - change, 0), max(y - change, 0), min(x + w + change, width), min(y + h + change, height))
+        im = im.crop(crop_tuple)
+        current_time = int(time.time())
+        faceImagePath = 'photos/%d-%d.png' % (current_time, i)
+        im.save(faceImagePath)
+
     return faces, height, width
