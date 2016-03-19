b55ac69abbaa522e51c5c4c5f2385eb1d0d74c42
Andrzej <andrzejjac@gmail.com>
2016-03-19 01:44:26 +0100

    added subpages

--- server/classlist_server/frontend/urls.py
+++ server/classlist_server/frontend/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^students$', views.students, name='students'),
+    url(r'^lessons$', views.lessons, name='lessons'),
+    url(r'^how_it_works$', views.how_it_works, name='how_it_works'),
 ]
--- server/classlist_server/frontend/views.py
+++ server/classlist_server/frontend/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse(render(request, 'index.html'))
+
+def students(request):
+    return HttpResponse(render(request, 'students.html'))
+
+def lessons(request):
+    return HttpResponse(render(request, 'lessons.html'))
+
+def how_it_works(request):
+    return HttpResponse(render(request, 'how_it_works.html'))
